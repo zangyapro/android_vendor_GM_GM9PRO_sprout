a68f42681eb59712dd43e364b6da2f27f77580b5
Ömer Aşıklı <104590063+zangyapro@users.noreply.github.com>
2022-07-09 14:13:50 +0300

    Update GM9PRO_sprout-vendor.mk

--- GM9PRO_sprout-vendor.mk
+++ GM9PRO_sprout-vendor.mk
@@ -7,6 +7,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_COPY_FILES += \
     vendor/GM/GM9PRO_sprout/proprietary/lib/fm_helium.so:$(TARGET_COPY_OUT_SYSTEM)/lib/fm_helium.so \
+    vendor/GM/GM9PRO_sprout/proprietary/lib/libjson.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libjson.so \
+    vendor/GM/GM9PRO_sprout/proprietary/lib/libtinyxml.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libtinyxml.so \ 
     vendor/GM/GM9PRO_sprout/proprietary/lib/libFileMux.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libFileMux.so \
     vendor/GM/GM9PRO_sprout/proprietary/lib/libOmxMux.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libOmxMux.so \
     vendor/GM/GM9PRO_sprout/proprietary/lib/libantradio.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libantradio.so \
@@ -17,6 +19,8 @@ PRODUCT_COPY_FILES += \
     vendor/GM/GM9PRO_sprout/proprietary/lib/libmmrtpencoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libmmrtpencoder.so \
     vendor/GM/GM9PRO_sprout/proprietary/lib/libsdm-disp-apis.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsdm-disp-apis.so \
     vendor/GM/GM9PRO_sprout/proprietary/lib64/fm_helium.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/fm_helium.so \
+    vendor/GM/GM9PRO_sprout/proprietary/lib64/libjson.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libjson.so \
+     vendor/GM/GM9PRO_sprout/proprietary/lib64/libtinyxml.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libtinyxml.so \
     vendor/GM/GM9PRO_sprout/proprietary/lib64/libFileMux.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libFileMux.so \
     vendor/GM/GM9PRO_sprout/proprietary/lib64/libOmxMux.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libOmxMux.so \
     vendor/GM/GM9PRO_sprout/proprietary/lib64/libantradio.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libantradio.so \
